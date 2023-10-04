1e08ef047b1293e7dbb84cdbd44d10fea68f176b
Morgen Diaz <morgen@webslinger.works>
2023-10-03 22:11:53 -1000

    feat: basic player movement.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const GAME_CANVAS = document.querySelector(".game-canvas");
 const CONTEXT = GAME_CANVAS.getContext("2d");
-const GRAVITY = 0.2;
+const GRAVITY = 0.7;
 
 GAME_CANVAS.width = 1024;
 GAME_CANVAS.height = 576;
@@ -12,6 +12,7 @@ class Sprite {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
+    this.lastPressedKey;
   }
 
   draw(canvasContext) {
@@ -42,6 +43,18 @@ const player = new Sprite(playerConfig);
 const enemyConfig = { position: { x: 400, y: 100 }, velocity: { x: 0, y: 0 } };
 const enemy = new Sprite(enemyConfig);
 
+const KEYS = {
+  a: { pressed: false },
+  d: { pressed: false },
+  w: { pressed: false },
+  ArrowLeft: { pressed: false },
+  ArrowUp: { pressed: false },
+  ArrowRight: { pressed: false },
+};
+
+const fighterMovementSpeed = 5;
+const fighterJumpVelocity = 20;
+
 function animate() {
   CONTEXT.fillStyle = "black";
   CONTEXT.fillRect(0, 0, GAME_CANVAS.width, GAME_CANVAS.height);
@@ -49,7 +62,63 @@ function animate() {
   player.update(CONTEXT, GAME_CANVAS.height);
   enemy.update(CONTEXT, GAME_CANVAS.height);
 
+  player.velocity.x = 0;
+
+  if (KEYS.a.pressed && player.lastPressedKey === "a") {
+    player.velocity.x = -fighterMovementSpeed;
+  } else if (KEYS.d.pressed && player.lastPressedKey === "d") {
+    player.velocity.x = fighterMovementSpeed;
+  }
+
+  if (KEYS.w.pressed) {
+    KEYS.w.pressed = false;
+  }
+
+  enemy.velocity.x = 0;
+
+  if (KEYS.ArrowLeft.pressed && enemy.lastPressedKey === "ArrowLeft") {
+    enemy.velocity.x = -fighterMovementSpeed;
+  } else if (KEYS.ArrowRight.pressed && enemy.lastPressedKey === "ArrowRight") {
+    enemy.velocity.x = fighterMovementSpeed;
+  }
   window.requestAnimationFrame(animate);
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+  if (event.key in KEYS) {
+    KEYS[event.key].pressed = true;
+  }
+
+  switch (event.key) {
+    case "a":
+      player.lastPressedKey = event.key;
+      break;
+    case "w":
+      player.velocity.y = -fighterJumpVelocity;
+      break;
+    case "d":
+      player.lastPressedKey = event.key;
+      break;
+    case "ArrowLeft":
+      enemy.lastPressedKey = event.key;
+      break;
+    case "ArrowUp":
+      enemy.velocity.y = -fighterJumpVelocity;
+      break;
+    case "ArrowRight":
+      enemy.lastPressedKey = event.key;
+      break;
+    default:
+      if (event.key in KEYS) {
+        KEYS.lastPressed = event.key;
+      }
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  if (event.key in KEYS) {
+    KEYS[event.key].pressed = false;
+  }
+});
